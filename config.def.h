f2d803b8f4823ed43fa9f4cdb8fb40f7c1e1e9bb
LogicSkky <wangkffrank@qq.com>
2020-03-27 14:55:20 +0800

    changed some settings of the dmenu

--- config.def.h
+++ config.def.h
@@ -15,8 +15,10 @@ static const int showsystray             = 1;   /* 0 means no systray */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 0;   /* 0 means bottom bar */
 static const char *fonts[]               = { "Source Code Pro:size=12" };
-static const char dmenufont[]            = "Source Code Pro:size=11";
+static const char dmenufont[]            = "Source Code Pro:size=12";
 static const char dmenuword[]            = "dmenu";
+static const char dmenuborderwidth[]     = "2";
+static const char dmenulist[]            = "15";
 static const char col_gray1[]            = "#222222";
 static const char col_gray2[]            = "#444444";
 static const char col_gray3[]            = "#bbbbbb";
@@ -75,7 +77,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char        dmenumon[2]   = "0";   /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", dmenuword, NULL };
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", dmenuword, "-l", dmenulist, "-i", "-bw", dmenuborderwidth, NULL };
 static const char *termcmd[]     = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 
